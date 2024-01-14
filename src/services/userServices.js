c91d32ead13237d58909de209fb5588bae965bc3
evgeniy-shpyra <foksmix256@gmail.com>
2024-01-14 20:59:09 +0200

    userService: add logic for registration and login

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,10 +1,33 @@
+import { getUserRepo } from "../db/index.js"
+import { getHash } from "../utils/hash.js"
 
-export const registration = ({payload}) => {
-    // business logic 
-    return payload
-    
+
+export const registration = async (payload) => {
+    const userRepository = getUserRepo()
+
+    const password = payload.password
+    const login = payload.login
+    const nickName = payload.nickName
+
+    // TODO: add validation
+
+    const passwordHash = getHash(password)
+
+    await userRepository.create({password: passwordHash, login, nickName}) 
 } 
 
-export const login = () => {
-    // business logic 
+export const login = async (payload) => {
+    const userRepository = getUserRepo()
+
+    const password = payload.password
+    const login = payload.login
+
+    const passwordHash = getHash(password)
+
+    const res = await userRepository.getUser({password: passwordHash, login})
+
+    if(!res.length)
+        throw new Error("Incorrect login or password")
+   
+    return res[0]
 }
