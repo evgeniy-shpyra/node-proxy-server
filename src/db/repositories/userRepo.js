d0a87748429bef953497da97b09d94e34f25ec0f
evgeniy-shpyra <foksmix256@gmail.com>
2024-01-15 22:18:30 +0200

    add changing flag: isOnline

--- src/db/repositories/userRepo.js
+++ src/db/repositories/userRepo.js
@@ -1,19 +1,38 @@
 const repo = (sequelize) => ({
     create: async (payload) => {
-        await sequelize.models.User.create({ payload, isOnline: false })
+        
+        await sequelize.models.User.create({ ...payload, isOnline: false })
     },
-    getUser: async (payload) => {
+    loginUser: async (payload) => {
         const { password, login } = payload
-        const foundedUser = await sequelize.models.User.findAll({
+        const foundedUser = await sequelize.models.User.findOne({
             where: {
                 password,
                 login
             }
         })
 
+        if(foundedUser){
+            await sequelize.models.User.update({isOnline: true}, {
+                where: {
+                    id: foundedUser.id
+                } 
+            })
+        }
    
         return foundedUser
     },
+    getUserByHash: async (hash) => {
+
+        const foundedUser = await sequelize.models.User.findOne({
+            where: {
+                password: hash
+            }
+        })
+
+        return foundedUser
+    }
 })
 
 export default repo
+ 
