bbdf7908b982a989e9bf6087369277c3c5abe7e7
evgeniy-shpyra <foksmix256@gmail.com>
2024-01-15 23:22:47 +0200

    getOne and create logic to Product repo

--- src/db/repositories/productsRepo.js
+++ src/db/repositories/productsRepo.js
@@ -1,11 +1,28 @@
 
-const productGet = (sequelize) => (payload) => {
-    // logic
-    // console.log()
-}
-
 const productRepo = (sequelize) => ({
-    get: productGet(sequelize)
+    getOne: async (externalProductId, userId) => {
+        console.log(externalProductId, userId)
+        const foundedProduct = await sequelize.models.Product.findOne({
+            where: {
+                externalId: externalProductId,
+                UserId: userId
+            }
+        })
+
+        return foundedProduct
+    },
+    getByQuery: async (query) => {
+        
+    },
+    create: async (payload, userId) => {
+        const externalId = payload.id
+
+        await sequelize.models.Product.create({
+            externalId,
+            json: JSON.stringify(payload),
+            UserId: userId
+        })
+    }
 })
 
 export default productRepo
