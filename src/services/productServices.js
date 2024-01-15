e8947f0e9fd8c8c013bb521885536dec4f74a997
evgeniy-shpyra <foksmix256@gmail.com>
2024-01-15 23:23:54 +0200

    add fetching to api, get one and create one product service

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,3 +1,52 @@
-export const getProduct = () => {
-    // business logic 
+import { getUserRepo, getProductRepo } from "../db/index.js"
+
+const externalApiUrl = 'https://dummyjson.com/products'
+
+const fetchExternalProducts = async (query) => {
+   
+    const id = query.id ? `/${query.id}` : ''
+
+    const response = await fetch(`${externalApiUrl}${id}`).then((response) => response.json())
+
+    return response
+} 
+
+export const getProduct = async (payload, query, authHeader) => {
+    const productRepo = getProductRepo()
+    if(!authHeader){
+        throw new Error("User unauthorized")
+    }
+
+    const userRepository = getUserRepo()
+    const user = await userRepository.getUserByHash(authHeader)
+
+    if(!user || user.isOnline === false){
+        throw new Error("User unauthorized")
+    }
+
+    // const 
+
+    const limit = query.limit
+    const skip = query.skip
+    const externalId = query.id
+
+    const responseProducts = []
+
+    if(externalId){
+        const foundedProduct = await productRepo.getOne(externalId, user.id)
+        if(foundedProduct){
+            responseProducts.push(foundedProduct)
+        }
+        else{
+            const newProducts = await fetchExternalProducts({id: externalId})
+            responseProducts.push(newProducts)
+           
+            await productRepo.create(newProducts, user.id)
+        }
+    }
+    else if(skip, limit){
+
+    }
+
+    return responseProducts
 }
