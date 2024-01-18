85f8cb3ba2e90249871d766bd99a5bab6735905a
evgeniy-shpyra <foksmix256@gmail.com>
2024-01-18 23:20:58 +0200

    add create bulk, find by id interval from db

--- src/db/repositories/productsRepo.js
+++ src/db/repositories/productsRepo.js
@@ -1,35 +1,43 @@
+import { Op } from "sequelize"
 
 const productRepo = (sequelize) => ({
     getOne: async (externalProductId, userId) => {
-        
+     
         const foundedProduct = await sequelize.models.Product.findOne({
             where: {
                 externalId: externalProductId,
+                UserId: userId
+            },
+        })
+    
+        return foundedProduct
+    },
+    getByQuery: async ({startId, endId, userId}) => {
+        const foundedProduct = await sequelize.models.Product.findAll({
+            attributes: ['json', 'externalId'],
+            where: {
+                externalId: {
+                    [Op.between]: [startId, endId],    
+                },
                 UserId: userId
             }
         })
 
         return foundedProduct
     },
-    getByQuery: async (query) => {
-        const foundedProduct = await sequelize.models.Product.findAll({
-            ...query,
-            order: [
-                ['externalId', 'ASC']
-            ]
-        })
-
-        return foundedProduct
-    },
     createOne: async (payload, userId) => {
         const externalId = payload.id
-
+       
         await sequelize.models.Product.create({
             externalId,
             json: JSON.stringify(payload),
             UserId: userId
         })
-    }
+    },
+    createBulk: async (payload, userId) => {
+        const queryData = payload.map(product => ({json: JSON.stringify(product), UserId: userId, externalId: product.id}))
+        await sequelize.models.Product.bulkCreate(queryData)
+    },
 })
 
 export default productRepo
